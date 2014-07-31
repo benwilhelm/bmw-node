c2177ca44d4283fccab9cace9eecb8a29174df47
Ben Wilhelm <ben@doublebeamdesign.com>
2014-07-30 20:59:00 -0500

    Bit.ly shortener complete

--- cmd/shorten.js
+++ cmd/shorten.js
@@ -1,9 +1,26 @@
-var request = require('request');
+var config = require('../lib/config')
+  , request = require('request')
+  ;
 
 module.exports = {
   usage: "shorten <url>",
   description: "Shortens the given URL using bit.ly's api",
   action: function(url) {
-    console.log('here will I shorten %s', url);
+    var username = config.get('bitly_username');
+    var bitlyKey = config.get('bitly_api_key');
+    var encUrl = encodeURIComponent(url);
+    var reqUrl = "https://api-ssl.bitly.com/v3/shorten?access_token=";
+    reqUrl += bitlyKey;
+    reqUrl += "&longUrl=";
+    reqUrl += encUrl;
+
+    request(reqUrl, function(err, resp, body){
+      var obj = JSON.parse(body);
+      if (obj.status_code === 200) {
+        console.log(obj.data.url);
+      } else {
+        throw obj.status_code + ": " + obj.status_txt
+      }
+    });
   }
 }
